176fd174223b43190494c0ae66b1d8c29d40d8f7
TXBIT <shenlong203@gmail.com>
2020-07-25 21:35:55 +0700

    034 Single Post Display

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -12,6 +12,7 @@ const PostItem = ({
   addLike,
   removeLike,
   deletePost,
+  showActions,
 }) => (
   <div className="post bg-white p-1 my-1">
     <div>
@@ -25,39 +26,48 @@ const PostItem = ({
       <p className="post-date">
         Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
       </p>
-      <button
-        onClick={() => addLike(_id)}
-        type="button"
-        className="btn btn-light"
-      >
-        <i className="fas fa-thumbs-up"></i>{' '}
-        {likes.length > 0 && <span>{likes.length}</span>}
-      </button>
-      <button
-        onClick={() => removeLike(_id)}
-        type="button"
-        className="btn btn-light"
-      >
-        <i className="fas fa-thumbs-down"></i>
-      </button>
-      <Link to={`/post/${_id}`} className="btn btn-primary">
-        Discussion{' '}
-        {comments.length > 0 && (
-          <span className="comment-count">{comments.length}</span>
-        )}
-      </Link>
-      {!auth.loading && user === auth.user._id && (
-        <button
-          onClick={() => deletePost(_id)}
-          type="button"
-          className="btn btn-danger"
-        >
-          <i className="fas fa-times"></i>
-        </button>
+
+      {showActions && (
+        <Fragment>
+          <button
+            onClick={() => addLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i className="fas fa-thumbs-up"></i>{' '}
+            {likes.length > 0 && <span>{likes.length}</span>}
+          </button>
+          <button
+            onClick={() => removeLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i className="fas fa-thumbs-down"></i>
+          </button>
+          <Link to={`/posts/${_id}`} className="btn btn-primary">
+            Discussion{' '}
+            {comments.length > 0 && (
+              <span className="comment-count">{comments.length}</span>
+            )}
+          </Link>
+          {!auth.loading && user === auth.user._id && (
+            <button
+              onClick={() => deletePost(_id)}
+              type="button"
+              className="btn btn-danger"
+            >
+              <i className="fas fa-times"></i>
+            </button>
+          )}
+        </Fragment>
       )}
     </div>
   </div>
 );
+
+PostItem.defaultProps = {
+  showActions: true,
+};
 
 PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
--- src/reducers/post.js
+++ src/reducers/post.js
@@ -1,5 +1,6 @@
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
@@ -18,6 +19,8 @@ export default (state = initialState, action) => {
   switch (type) {
     case GET_POSTS:
       return { ...state, posts: payload, loading: false };
+    case GET_POST:
+      return { ...state, post: payload, loading: false };
     case ADD_POST:
       return { ...state, posts: [payload, ...state.posts], loading: false };
     case DELETE_POST:
